1fcdcfccf04a960b6349d4a5ad881fffcec195fa
Karen Gutierrez <kareng018@berkeley.edu>
2021-05-10 20:51:32 -0700

    committing after api keys

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "4168f760c28924c0f387f21c8a0c2599"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDWT4lPFD5VX2F5glnGguCtfKCQVym_Qxk"
